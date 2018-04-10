3a85500585f8280651425388696b5afd6f40d479
yingying <nicoledeyx@163.com>
2018-04-10 21:10:29 +0800

    websockt

---
+++ demo3/wsServer.js
@@ -0,0 +1,38 @@
+
+let ws = require("nodejs-websocket");
+
+let  PORT = 3000;
+
+let clientCount = 0;
+
+// Scream server example: "hi" -> "HI!!!"
+let server = ws.createServer(function (conn) {
+    console.log("New connection");
+
+    clientCount++;
+
+    conn.nickname = 'user' + clientCount;
+
+    broadcast(conn.nickname + 'comes in');
+
+    conn.on("text", function (str) {
+        console.log("Received "+str);
+        broadcast(str)
+    });
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed");
+        broadcast(conn.nickname + 'left');
+    });
+    conn.on("error", function (err) {
+        console.log("handle error");
+        console.log(err)
+    })
+}).listen(PORT);
+
+console.log("websocket server listening on port " + PORT);
+
+function broadcast(str) {
+    server.connections.forEach(function (connection) {
+        connection.sendText(str);
+    })
+}
---
+++ demo6/wsServer.js
@@ -0,0 +1,25 @@
+
+let app = require('http').createServer();
+let io = require('socket.io')(app);
+
+let  PORT = 3000;
+
+let clientCount = 0;
+
+app.listen(PORT);
+
+io.on('connection', function (socket) {
+    clientCount++;
+    socket.nickname = 'user' + clientCount;
+    io.emit('enter', socket.nickname + ' comes in');
+
+    socket.on('message', function (str) {
+        io.emit('message', socket.nickname + ' says: ' + str);
+    })
+
+    socket.on('disconnect', function () {
+        io.emit('leave', socket.nickname + ' left');
+    })
+});
+
+console.log("websocket server listening on port " + PORT);
